d4505e9de179bc1f7f0202bf8e40ba57544c987d
Alon David Jacobson <alojaco1@gmail.com>
2018-06-30 00:42:02 +0000

    snake game displays score at Game Over

--- snake/snake.js
+++ snake/snake.js
@@ -4,7 +4,8 @@ var direction = "right";
 var row = 7;
 var col = 9;
 var draws = 0;
-var snakeLength = 3;
+var startingSnakeLength = 3;
+var snakeLength = startingSnakeLength;
 var snake = [];
 var gameOver = false;
 var egg;
@@ -13,65 +14,69 @@ var score;
 function setup() {
   var canvas = createCanvas(570, 450);
   canvas.parent("right");
-  snake.push([col - 2, row]);
-  snake.push([col - 1, row]);
-  snake.push([col, row]);
+  for (var i = startingSnakeLength - 1; i >= 0; i--) {
+    snake.push([col - i, row]);
+  }
   egg = [int(random(cols)), int(random(rows))];
   textSize(30);
+  strokeWeight(4);
+  stroke(114);
+}
+
+function drawSquare(pos) {
+  rect(pos[0] * width / cols, pos[1] * height / rows, width / cols, height / rows);
 }
 
 function draw() {
   background(114);
-  stroke(114);
-  strokeWeight(4);
-  fill(0);
-  if(!gameOver) {
+  if (! gameOver) {
     draws++;
-    if (draws % 9 == 0) {
-      if (direction == "up")
+    if (draws % 9 === 0) {
+      if (direction === "up")
         row--;
-      else if (direction == "down")
+      else if (direction === "down")
         row++;
-      else if (direction == "left")
+      else if (direction === "left")
         col--;
-      else if (direction == "right")
+      else if (direction === "right")
         col++;
 
-      if(col == egg[0] && row == egg[1]) {
+      if(col === egg[0] && row === egg[1]) {
         egg = [int(random(cols)), int(random(rows))];
         snakeLength++;
       }
 
-      gameOver = col < 0 || col >= cols || row < 0 || row >= rows;
-
-      if(snake.length == snakeLength)
+      if (snake.length === snakeLength)
         snake.splice(0, 1);
-      snake.push([col, row]);
 
-      for(var i = 0; i < snake.length - 1; i++) {
-        if(snake[i][0] == col && snake[i][1] == row)
-          gameOver = true;
-      }
+      var headOnEdge = col < 0 || col >= cols || row < 0 || row >= rows;
+      var headOnSelf = snake.some(element => element[0] === col && element[1] === row);
+      gameOver = headOnEdge || headOnSelf;
+
+      snake.push([col, row]);
     }
+
+    fill(0);
     for (let pos of snake) {
-      rect(pos[0] * width / cols, pos[1] * height / rows, width / cols, height / rows);
+      drawSquare(pos);
     }
-    rect(egg[0] * width / cols, egg[1] * height / rows, width / cols, height / rows);
+    drawSquare(egg);
 
     fill(255);
-    text("score: " + (snakeLength - 3), 10, 30);
+    score = snakeLength - startingSnakeLength;
+    text("score: " + score, 10, 30);
   } else {
-    text("Game Over", 10, 30);
+    text("Game Over. Score: " + score, 10, 30);
   }
 }
 
 function keyPressed() {
-  if (keyCode == UP_ARROW && direction != "down")
+  if (keyCode === UP_ARROW && direction !== "down")
     direction = "up";
-  else if (keyCode == DOWN_ARROW && direction != "up")
+  else if (keyCode === DOWN_ARROW && direction !== "up")
     direction = "down";
-  else if (keyCode == LEFT_ARROW && direction != "right")
+  else if (keyCode === LEFT_ARROW && direction !== "right")
     direction = "left";
-  else if (keyCode == RIGHT_ARROW && direction != "left")
+  else if (keyCode === RIGHT_ARROW && direction !== "left")
     direction = "right";
 }
